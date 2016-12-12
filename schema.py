17305515b1bafd82c1beea37b88edbfe08355f72
Jason Lane <quizathaderat@yahoo.com>
2016-12-11 22:19:56 -0800

    Add ability to find employee by name

--- schema.py
+++ schema.py
@@ -1,3 +1,6 @@
+#import code
+#code.interact(local=dict(globals(), **locals()))
+
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
@@ -19,5 +22,13 @@ class Employee(SQLAlchemyObjectType):
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_employees = SQLAlchemyConnectionField(Employee)
+    employee = graphene.Field(Employee, name=graphene.String())
+
+    def resolve_employee(self, args, info, x):
+        employees = EmployeeModel.query.all()
+
+        for e in employees:
+            if e.name == args.get("name"):
+                return e
 
 schema = graphene.Schema(query=Query)
